85cf9f2d427f157c12432ecccaca240da27b07fe
kelvindai1994 <kelvindai1994@gmail.com>
2022-12-07 23:09:50 +0700

    no message

---
+++ Assets/_TOA/Scripts/Map/Gate.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gate : MonoBehaviour
+{
+    public GameObject portal;
+
+    private void Start()
+    {
+        portal.SetActive(false);
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
+            portal.SetActive(true);
+        }
+    }
+}
--- Assets/_TOA/Scripts/Scene/SceneIndex.cs
+++ Assets/_TOA/Scripts/Scene/SceneIndex.cs
@@ -3,5 +3,6 @@ public enum SceneIndex
     Menu = 0,
     CharacterSelection = 1,
     TownMap = 2,
-    Map1 = 3
+    Map1 = 3,
+    Map2 = 4
 }
